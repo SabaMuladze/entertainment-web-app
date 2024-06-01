9ece2500cc195a6c203b6adf23b576e9bb8c8594
SabaMuladze <muladze66@bk.ru>
2024-06-01 20:18:43 +0400

    install tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 
 <template>
-  
+  <div class=" bg-red-400">Saba</div>
 </template>
 
 <script >
 </script>
 
 <style scoped>
-
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box
+}
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 </style>
