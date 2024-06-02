c652be338f8b15a0c8a02dc90dc63bce0db96f06
SabaMuladze <muladze66@bk.ru>
2024-06-02 23:22:10 +0400

    styles for tablet&desktop

--- src/components/auth/UserAuth.vue
+++ src/components/auth/UserAuth.vue
@@ -8,17 +8,19 @@
         />
       </svg>
     </header>
-    <div class="flex flex-col p-7 bg-cusdarkblue rounded-md gap-3 justify-center mt-12 w-full  md:max-w-[400px]">
-        <h2 class="text-3xl">Login</h2>
-        <form class="flex flex-col gap-3 mt-3">
+    <div class="flex flex-col p-7 bg-cusdarkblue rounded-md gap-3 justify-center mt-12 w-full  md:max-w-[400px] md:p-10 md:mt-20 md:gap-5">
+        <h2 v-if="mode == 'login'" class="text-3xl md:text-4xl">Login</h2>
+        <h2 v-else class="text-3xl md:text-4xl">Sign Up</h2>
+
+        <form @submit.prevent="" class="flex flex-col gap-3 mt-3 md:gap-5">
             <div class=" border-b-cusgrey border-b-[1px]">
-                <input class=" outline-none bg-transparent h-10" type="email" placeholder="Email Address">
+                <input class=" outline-none bg-transparent h-10 w-full" type="email" placeholder="Email Address">
             </div>
             <div class=" border-b-cusgrey border-b-[1px]">
-                <input class=" outline-none bg-transparent h-10" type="password" placeholder="Password">
+                <input class=" outline-none bg-transparent h-10 w-full" type="password" placeholder="Password">
             </div>
             <div class=" border-b-cusgrey border-b-[1px]" v-if=" mode == 'signup'">
-                <input class=" outline-none bg-transparent h-10" type="password" placeholder="Repeat Password">
+                <input class=" outline-none bg-transparent h-10 w-full" type="password" placeholder="Repeat Password">
             </div>
             <button class="bg-cusred mt-3 py-3 rounded-lg hover:bg-white hover:text-black">Login to your account</button>
         </form>
