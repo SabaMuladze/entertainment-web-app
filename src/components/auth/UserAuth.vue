1d58686add5a05f5460f6fc7f72b8a54bbb71ba7
SabaMuladze <muladze66@bk.ru>
2024-06-02 22:59:42 +0400

    add basic styles on auth

--- src/components/auth/UserAuth.vue
+++ src/components/auth/UserAuth.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col justify-center items-center">
+  <div class="flex flex-col  items-center h-[100vh] p-6">
     <header class="flex justify-center items-center">
       <svg class="mt-16" width="33" height="27" xmlns="http://www.w3.org/2000/svg">
         <path
@@ -8,12 +8,33 @@
         />
       </svg>
     </header>
-    <div>
-        <h2></h2>
-        <form></form>
-        <p></p>
+    <div class="flex flex-col p-7 bg-cusdarkblue rounded-md gap-3 justify-center mt-12 w-full  md:max-w-[400px]">
+        <h2 class="text-3xl">Login</h2>
+        <form class="flex flex-col gap-3 mt-3">
+            <div class=" border-b-cusgrey border-b-[1px]">
+                <input class=" outline-none bg-transparent h-10" type="email" placeholder="Email Address">
+            </div>
+            <div class=" border-b-cusgrey border-b-[1px]">
+                <input class=" outline-none bg-transparent h-10" type="password" placeholder="Password">
+            </div>
+            <div class=" border-b-cusgrey border-b-[1px]" v-if=" mode == 'signup'">
+                <input class=" outline-none bg-transparent h-10" type="password" placeholder="Repeat Password">
+            </div>
+            <button class="bg-cusred mt-3 py-3 rounded-lg hover:bg-white hover:text-black">Login to your account</button>
+        </form>
+        <p class="text-center">Don't have an account? <span class="text-cusred">signup</span></p>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+export default{
+    data() {
+        return {
+            mode:'login',
+            email:'',
+            password:'',
+        }
+    },
+}
+</script>
