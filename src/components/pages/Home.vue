7fe459b650dede6acfb191541692f7f4220e455f
SabaMuladze <muladze66@bk.ru>
2024-06-04 22:42:34 +0400

    slider with swiper.js

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,14 +1,31 @@
 <template>
-    <div class="w-full">
+    <div class=" xl:w-[90%] p-6 xl:pl-0 ">
         <div>
-            <ul>
-                <li v-for="(trending) in moviesData" :key="trending.title">
-                    <div>
-                        <img v-if="trending.isTrending == true" :src="trending.thumbnail.trending.small" alt="">
-                        <!-- <p v-if="trending.isTrending == true">{{ trending.thumbnail.trending.small }}</p> -->
-                    </div>
-                </li>
-            </ul>
+            <swiper 
+    :spaceBetween="10"
+    :onReachEnd="5"
+    :pagination="{
+      clickable: true,
+    }"
+    :modules="modules"
+    :breakpoints="{
+      '@0.00': {
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+      },
+      '@0.75': {
+        slidesPerView: 2.2,
+        spaceBetween: 20,
+      },
+      }"
+    class="mySwiper"
+             >
+                <SwiperSlide v-for="(trending) in moviesData" :key="trending.title">
+                        <img class="rounded-lg min-w-[230px] max-w-[260px] md:hidden" v-if="trending.isTrending == true && small " :src="trending.thumbnail.trending.small" alt="">
+                        <img class="rounded-lg hidden" v-if="trending.isTrending == true && large" :src="trending.thumbnail.trending.large" alt="">
+
+                </SwiperSlide>
+            </swiper>
         </div>
         <div></div>
     </div>
@@ -19,15 +36,28 @@ import { Swiper, SwiperSlide } from 'swiper/vue';
 import data from '../../../data.json'
 import 'swiper/css';
 import '../../styles.css';
+
 export default{
     data() {
         return {
-            moviesData:data
+            moviesData:data,
         }
     },
     components:{
         Swiper,
         SwiperSlide
+    },
+    computed:{
+        small(){
+           return screen.width <= 768
+        },
+        large(){
+           return screen.width >= 768
+        }
+    },
+    mounted(){
+
+        console.log(screen.width < 768);
     }
 }
 </script>
