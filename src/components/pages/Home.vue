d9c35b34fd6ccc7b19603b7541eded70b330fc0f
SabaMuladze <muladze66@bk.ru>
2024-06-04 20:19:14 +0400

    dispay trending movies

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="w-full">
+        <div>
+            <ul>
+                <li v-for="(trending) in moviesData" :key="trending.title">
+                    <div>
+                        <img v-if="trending.isTrending == true" :src="trending.thumbnail.trending.small" alt="">
+                        <!-- <p v-if="trending.isTrending == true">{{ trending.thumbnail.trending.small }}</p> -->
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import data from '../../../data.json'
+import 'swiper/css';
+import '../../styles.css';
+export default{
+    data() {
+        return {
+            moviesData:data
+        }
+    },
+    components:{
+        Swiper,
+        SwiperSlide
+    }
+}
+</script>
+
+
+<!-- <Swiper>
+                <swiper-slide v-for="(trending, index) in moviesData" :key="index">
+                    <div>
+                        <img :src="trending?.thumbnail?.trending.small" alt="">
+                    </div>
+                </swiper-slide>
+            </Swiper> -->
--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import { createRouter,createWebHistory } from "vue-router";
 import UserAuth from './components/auth/UserAuth.vue'
+import Home from './components/pages/Home.vue'
 
 const router = createRouter({
     history:createWebHistory(),
     routes:[
-        {path:'/auth',component:UserAuth}
+        {path:'/auth',component:UserAuth},
+        {path:'/home',component:Home},
+
     ]
 })
 
