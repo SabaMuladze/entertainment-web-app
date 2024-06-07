8e97215e73b46580de9af0880ca4fff13bc7e98f
SabaMuladze <muladze66@bk.ru>
2024-06-08 00:38:15 +0400

    add category icons

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -29,7 +29,10 @@
                         <img class="rounded-lg  md:hidden" v-if="trending.isTrending == true && small " :src="trending.thumbnail.trending.small" alt="">
                         <img class="rounded-lg max-md:hidden " v-else-if="trending.isTrending == true && large" :src="trending.thumbnail.trending.large" alt="">
                         <div class=" w-full absolute bottom-0 p-5 rounded-lg  bg-gradient-to-b from-transparent  to-black ">
-                            <p class="text-[12px] md:text-[16px] text-gray-400">{{ trending.year}} {{ trending.category }} {{ trending.rating }}</p>
+                            <p class="text-[12px] md:text-[16px] text-gray-400 flex items-center gap-2">{{ trending.year}}
+                               <svg v-if="trending.category == 'Movie'"  width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M12 2.689H5.448L7.068.722 6.132 0 4.2 2.345 2.268.017l-.936.705 1.62 1.967H0V12h12V2.689Zm-4.8 8.147h-6V3.853h6v6.983Zm3-2.328H9V7.344h1.2v1.164Zm0-2.328H9V5.016h1.2V6.18Z" fill="#FFF" opacity=".75"/></svg>
+                               <svg v-else  width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M10.173 0H1.827A1.827 1.827 0 0 0 0 1.827v8.346C0 11.183.818 12 1.827 12h8.346A1.827 1.827 0 0 0 12 10.173V1.827A1.827 1.827 0 0 0 10.173 0ZM2.4 5.4H1.2V4.2h1.2v1.2ZM1.2 6.6h1.2v1.2H1.2V6.6Zm9.6-1.2H9.6V4.2h1.2v1.2ZM9.6 6.6h1.2v1.2H9.6V6.6Zm1.2-4.956V2.4H9.6V1.2h.756a.444.444 0 0 1 .444.444ZM1.644 1.2H2.4v1.2H1.2v-.756a.444.444 0 0 1 .444-.444ZM1.2 10.356V9.6h1.2v1.2h-.756a.444.444 0 0 1-.444-.444Zm9.6 0a.444.444 0 0 1-.444.444H9.6V9.6h1.2v.756Z" fill="#FFF" opacity=".75"/></svg>
+                                 {{ trending.category }} {{ trending.rating }}</p>
                             <h3 class="text-[15px] md:text-[28px]" > {{ trending.title }}</h3>
                         </div>
                 </SwiperSlide>
@@ -42,7 +45,10 @@
                     <img class="rounded-lg  md:hidden"  :src="recommended.thumbnail.regular.small" alt="">
                     <img class="rounded-lg max-md:hidden " :src="recommended.thumbnail.regular.medium" alt="">
                         <div class=" w-full rounded-lg   ">
-                            <p class="text-[10px] md:text-[14px] text-gray-400">{{ recommended.year}} {{ recommended.category }} {{ recommended.rating }}</p>
+                            <p class="text-[10px] md:text-[14px] text-gray-400 flex items-center gap-2">{{ recommended.year}}
+                                <img v-if="recommended.category == 'Movie'" src="../../../public/icon-category-movie.svg" alt="">
+                                <img v-else src="../../../public/icon-category-tv.svg" alt="">
+                                 {{ recommended.category }} {{ recommended.rating }}</p>
                             <h3 class="text-[15px] md:text-[18px] font-semibold" > {{ recommended.title }}</h3>
                         </div>
                 </div>
