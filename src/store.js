23979736796cb4d435779e1622b68e79b69683f5
SabaMuladze <muladze66@bk.ru>
2024-06-16 19:54:10 +0400

    bookmark on each movie

--- src/store.js
+++ src/store.js
@@ -1,1 +1,34 @@
-import { createStore } from "vuex/types/index.js";
+import { createStore } from "vuex";
+import  data  from "../data.json";
+
+const store = createStore({
+  state: {
+    count: 0,
+    bookmarkedData:[],
+    cardData:data
+  },
+  mutations: {
+    bookmark(state,recommended) {
+      const items = state.cardData.find(i => i == recommended);
+      if(items.title == recommended.title){
+if (typeof items.marked === 'undefined') {
+        items.marked = true
+      }
+      else  {
+        items.marked = !items.marked      
+      }
+      console.log(items.title,recommended.title);
+    }
+      }
+  },
+  actions: {
+    bookmark(context,recommended) {
+      context.commit('bookmark',recommended);
+    }
+  },
+  getters: {
+    marked: state => state.marked
+  }
+});
+
+export default store;
